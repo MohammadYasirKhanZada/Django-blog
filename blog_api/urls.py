3d5987cf80518803d12829f4c6bdd9c2a2b6d786
Mohammad Yasir <m.yasirkhanzada99@gmail.com>
2023-12-21 19:33:07 +0500

    [blog], [blog_api], [core] and other update.

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -1,9 +1,12 @@
-# all urls.py code will go here.
+# All urls.py code will go here.
 # Folder blog_api own urls.py file.
 
 from django.urls import path
+from .views import PostList, PostDetail
 
 app_name = "blog_api"
 
 urlpatterns = [
+    path('<int:pk>/', PostDetail.as_view(), name="detailcreate"),
+    path('', PostList.as_view(), name='listcreate')
 ]
--- blog_api/views.py
+++ blog_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
+from blog.models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+# PostList View
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.postobjects.all()
+    serializer_class = PostSerializer
+    pass
+
+# PostDetail View
+class PostDetail(generics.RetrieveDestroyAPIView):
+    queryset = Post.postobjects.all()
+    serializer_class = PostSerializer
+    pass
